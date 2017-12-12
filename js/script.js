66d98651d0bc5bec81f5146d132da60138977dba
p14164076 <p14164076>
2017-12-12 17:21:56 +0000

    Added map and fixed some errors

--- js/script.js
+++ js/script.js
@@ -46,4 +46,23 @@ function clickAlert() {
     alert("Thank you \n Your form has been submitted");
 }
 
+function initMap() {
+  let leicester = {lat: 52.631236, lng: -1.125161};
+  let fortacc = {lat: 52.631045, lng: -1.12362};
+  let mapDemo = document.getElementById("map");
+
+  // this sets the default location for when the map is first loaded
+  let map = new google.maps.Map(mapDemo, {
+    zoom: 16,
+    center: leicester
+  });
+
+  // these set different markers you want to show on your map
+  let markerDMU = new google.maps.Marker({
+    position: fortacc,
+    map: map,
+    title: 'Fortress Accountacy'
+  });
+}
+
 
