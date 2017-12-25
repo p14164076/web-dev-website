cf07a76fcea52736ca5a46a4dc39b2e8f294a84d
p14164076 <p14164076>
2017-12-25 23:08:36 +0000

    Fixed Navigation smooth scrolling

--- js/script.js
+++ js/script.js
@@ -42,17 +42,7 @@ function initMap() {
   });
 }
 
-var win      = $(window),
-    fxel     = $('nav'),
-    eloffset = fxel.offset().top;
 
-win.scroll(function() {
-    if (eloffset < win.scrollTop()) {
-        fxel.addClass("fixed");
-    } else {
-        fxel.removeClass("fixed");
-    }
-});
 
 
 
