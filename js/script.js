208d7a689313dcae7137c395985a66593741dda7
p14164076 <p14164076>
2017-12-28 01:41:17 +0000

    Validation and cleaned up code

--- js/script.js
+++ js/script.js
@@ -1,51 +1,42 @@
-	(function(){
+	(function() {
+	  let menuButton = document.getElementById("menu");
+	  let navMenu = document.getElementById("nav-menu");
+	  menuButton.addEventListener("click", toggleMenu);
+	  let toggle = false; // hidden at first
+	  function toggleMenu() {
+	    if (toggle) { // true: it's visible
+	      navMenu.classList.remove("show-menu"), // hide it
+	        toggle = false
+	    } else { // false: it's hidden
+	      navMenu.classList.add("show-menu"), // show it
+	        toggle = true
+	    }
+	  }
+	})();
 
-  let menuButton = document.getElementById("menu");
-  let navMenu = document.getElementById("nav-menu");
+	function clickAlert() {
+	  alert("Thank you \n Your form has been submitted");
+	}
 
-  menuButton.addEventListener("click", toggleMenu);
-
-  let toggle = false; // hidden at first
-  function toggleMenu(){
-    if (toggle) { // true: it's visible
-      navMenu.classList.remove("show-menu"), // hide it
-      toggle = false
-    }
-    else { // false: it's hidden
-      navMenu.classList.add("show-menu"), // show it
-      toggle = true
-    }
-  }
-
-})();
-
-function clickAlert() {
-    alert("Thank you \n Your form has been submitted");
-}
-
-function initMap() {
-  let leicester = {lat: 52.631236, lng: -1.125161};
-  let fortacc = {lat: 52.631045, lng: -1.12362};
-  let mapDemo = document.getElementById("map");
-
-  // this sets the default location for when the map is first loaded
-  let map = new google.maps.Map(mapDemo, {
-    zoom: 16,
-    center: leicester
-  });
-
-  // these set different markers you want to show on your map
-  let markerDMU = new google.maps.Marker({
-    position: fortacc,
-    map: map,
-    title: 'Fortress Accountacy'
-  });
-}
-
-
-
-
-
-
-
-
+	function initMap() {
+	  let leicester = {
+	    lat: 52.631236,
+	    lng: -1.125161
+	  };
+	  let fortacc = {
+	    lat: 52.631045,
+	    lng: -1.12362
+	  };
+	  let mapDemo = document.getElementById("map");
+	  // this sets the default location for when the map is first loaded
+	  let map = new google.maps.Map(mapDemo, {
+	    zoom: 16,
+	    center: leicester
+	  });
+	  // these set different markers you want to show on your map
+	  let markerDMU = new google.maps.Marker({
+	    position: fortacc,
+	    map: map,
+	    title: 'Fortress Accountacy'
+	  });
+	}
